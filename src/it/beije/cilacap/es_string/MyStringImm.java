d2919a4a0538723bb5f5d3d9aef11e3726e5dfa7
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-01-15 15:16:37 +0100

    Initial Padawan09 commit

---
+++ src/it/beije/cilacap/es_string/MyStringImm.java
@@ -0,0 +1,60 @@
+package it.beije.cilacap.es_string;
+
+public class MyStringImm {
+	
+	private String string;
+	
+	public MyStringImm(String string) {
+		this.string = string;
+	}
+	
+	public String toString() {
+		return string;
+	}
+	
+	public boolean myContains(String contenuta) {
+		if ( string.indexOf(contenuta) >= 0 ) return true;
+		else return false;
+	}
+	
+	public boolean myStartsWith(String begin) {
+		if ( string.indexOf(begin) == 0 ) return true;
+		else return false;
+	}
+	
+	public boolean myEndsWith(String end) {
+		if ( string.indexOf(end) == string.length()-end.length() ) return true;
+		else return false;
+	}
+	
+	public MyStringImm myTrim() {	
+		int i, j;
+		for( i=0; i<string.length() && string.charAt(i)==' '; i++);
+		for( j=string.length()-1; j>=0 && string.charAt(j)==' '; j--);
+		return mySubstring(i, ++j);
+	}
+	
+	public MyStringImm myReplace(char oldC, char newC) {
+		String rep = "";
+		for ( int i=0; i<string.length(); i++ ) {
+			if ( string.charAt(i) == oldC ) rep += newC;
+			else rep += string.charAt(i);
+		}
+		return new MyStringImm(rep);
+	}
+	
+	public boolean myEquals(String s) {
+		if ( s.length() != string.length() ) return false;
+		for( int i = 0; i<s.length(); i++) {
+			if( s.charAt(i) != string.charAt(i) ) return false;
+		}
+		return true;
+	}
+	
+	public MyStringImm mySubstring(int start, int end) {
+		String s="";
+		if ( start > end || end > string.length() ) return null;
+		for( int i = start; i<end; i++) s+=string.charAt(i);
+		return new MyStringImm(s);
+	}
+}
---
+++ src/it/beije/cilacap/es_string/MyStringMut.java
@@ -0,0 +1,63 @@
+package it.beije.cilacap.es_string;
+
+public class MyStringMut {
+	
+	private String string;
+	
+	public MyStringMut(String string) {
+		this.string = string;
+	}
+	
+	public String toString() {
+		return string;
+	}
+	
+	public boolean myContains(String contenuta) {
+		if ( string.indexOf(contenuta) >= 0 ) return true;
+		else return false;
+	}
+	
+	public boolean myStartsWith(String begin) {
+		if ( string.indexOf(begin) == 0 ) return true;
+		else return false;
+	}
+	
+	public boolean myEndsWith(String end) {
+		if ( string.indexOf(end) == string.length()-end.length() ) return true;
+		else return false;
+	}
+	
+	public MyStringMut myTrim() {	
+		int i, j;
+		for( i=0; i<string.length() && string.charAt(i)==' '; i++);
+		for( j=string.length()-1; j>=0 && string.charAt(j)==' '; j--);
+		string = mySubstring(i, ++j).toString();
+		return this;
+	}
+	
+	public MyStringMut myReplace(char oldC, char newC) {
+		String rep = "";
+		for ( int i=0; i<string.length(); i++ ) {
+			if ( string.charAt(i) == oldC ) rep += newC;
+			else rep += string.charAt(i);
+		}
+		string = rep;
+		return this;
+	}
+	
+	public boolean myEquals(String s) {
+		if ( s.length() != string.length() ) return false;
+		for( int i = 0; i<s.length(); i++) {
+			if( s.charAt(i) != string.charAt(i) ) return false;
+		}
+		return true;
+	}
+	
+	public MyStringMut mySubstring(int start, int end) {
+		String s="";
+		if ( start > end || end > string.length() ) return null;
+		for( int i = start; i<end; i++) s+=string.charAt(i);
+		string = s;
+		return this;
+	}
+}
