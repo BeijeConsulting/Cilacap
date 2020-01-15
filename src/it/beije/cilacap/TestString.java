c966a524d1e378919933c71e5e7086b201d7d5cf
Padawan03 <Padawan03@DESKTOP-EKKM993>
2020-01-15 16:19:41 +0100

    Esercizi completi

--- src/it/beije/cilacap/TestString.java
+++ src/it/beije/cilacap/TestString.java
@@ -7,11 +7,13 @@ public class TestString {
 		
     TestString ms = new TestString();
 	String prova = "bellaziobella";
-	
+	String buona = new String();
 	boolean provina = ms.Mycontains(prova,"bella");
 	
 	System.out.println(provina);
-		
+	buona = ms.Reverse(prova);
+	System.out.println(buona);
+	
 		
 		
 		
@@ -42,32 +44,77 @@ public class TestString {
 	}
 	
 	public boolean MyStartWhit(String s,String s1) {
-
-		return false;
+		if(s.indexOf(s1,0)!=-1) {
+			return true;
+		}else return false;	
+	}
+	
+	public boolean MyEndWhit(String s,String s1) {
+		int we = s.length() - s1.length();
+		if(s.indexOf(s1,we)!=-1) {
+			return true;
+		}else return false;
 		
 	}
-	public boolean MyEndWhit(String s) {
+	
+	public boolean MyEquals(String s,String s1) {
+		if(s.length()==s1.length()) {
+			for(int i=0;i<s1.length();i++) {
+				if(s.charAt(i)!=s1.charAt(i)) {
+					return false;
+				}
+			}
+			return false;
+		}
 		return false;
-		
 	}
+	
+	public String Myreplace(String parola,char s1,char s2) {
+		String nuova = new String();
+		for(int i=0;i<parola.length();i++) {
+			if(parola.charAt(i)==s1) {
+				nuova+=s2;
+			}else {
+				nuova+=parola.charAt(i);
+			}
+		}
+		return nuova;	
+	}
+	
+	public String MySubString(String s,int startIndex,int endIndex) {
+		String buona = new String();
+		for(int i=startIndex;i<endIndex;i++) {
+			buona += s.charAt(i);
+		}
+		return buona;	
+	}
+	
+	public String Reverse(String s) {
+		String nuova = new String();
+		for(int i=s.length()-1;i>=0;i--)
+			nuova += s.charAt(i);
+		return nuova;
+	}
+	
 	public String MyTrim(String s) {
-		return s;
-		
-		
-	}
-	public boolean MyEquals(String s) {
-		return false;
-		
-	}
-	public String Myreplace(String s) {
-		return s;
-		
-	}
-	
-	public String MySubString(int startIndex,int endIndex) {
-		return null;
-	
-		
+		String buona = new String();
+		int capo=0, coda = 0;
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)!=' ') {
+				capo = i;
+				break;
+			}
+		}
+		for(int i=s.length()-1;i>=0;i--) {
+			if(s.charAt(i)!=' ') {
+				coda=i;
+				break;
+			}
+		}
+		for(int i=capo;i<=coda;i++) {
+			buona+=s.charAt(i);
+		}
+		return buona;	
 	}
 
 }
