68533ee97a5e6c589b2465d13751180602ea9fc7
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-01-16 18:03:52 +0100

    Padawan09 Commit 16/01

--- src/it/beije/cilacap/es_morra_cinese/MorraMain.java
+++ src/it/beije/cilacap/es_morra_cinese/MorraMain.java
@@ -8,7 +8,7 @@ public class MorraMain {
 	public static void main(String[] args) {
 		int scegli = 0;
 		while(true){
-			System.out.println("premi 1 per 1vs1, 2 per 1vsCPU");
+			System.out.println("premi 1 per 1vs1, 2 per 1vsCPU\n(0 per sasso, 1 per carta, 2 per forbice)");
 			 scegli = sc.nextInt();
 			 if(scegli==1) partita1vs1();
 			 if(scegli==2) partita1vsCPU();
@@ -32,23 +32,43 @@ public class MorraMain {
 	}
 	
 	private static void partita(int a, int b) {
+		int c = vincitore(a,b);
 		if (a%3>b%3) System.out.println("Vince giocatore 1!");
 		else if (a%3<b%3) System.out.println("Vince giocatore 2!");
 		else System.out.println("Pareggio!");
 	}
 	
 	private static void partitaComp(int a, int b) {
-		if (a%3>b%3) System.out.println("Vince giocatore 1 con "+getType(a)+" su "+getType(b));
-		else if (a%3<b%3) System.out.println("Vince giocatore 2 con "+getType(b)+" su "+getType(a));
-		else System.out.println("Pareggio con "+getType(a));
+		int c = vincitore(a,b);
+		if (c==1) System.out.println("Vince giocatore 1 con "+getType(a)+" su "+getType(b));
+		else if (c==2) System.out.println("Vince giocatore 2 con "+getType(b)+" su "+getType(a));
+		else if (c==0) System.out.println("Pareggio con "+getType(a));
 	}
 	
 	private static String getType(int a) {
 		switch(a%3) {
-		case 0: return "Forbice";
-		case 1: return "Sasso";
-		case 2: return "Carta";
+		case 0: return "Sasso";
+		case 1: return "Carta";
+		case 2: return "Forbice";
 		default: return "";
+		}
+	}
+	
+	private static int vincitore(int a, int b) {
+		if(a==b) return 0;
+		switch (a) {
+			case 2: 
+				if(b==1) return 1;
+				else return 2;
+			case 1: 
+				if(b==0) return 1;
+				else return 2;
+			case 0:
+				if(b==2) return 1;
+				else return 2;
+			default:
+				return -1;
+				
 		}
 	}
 }
