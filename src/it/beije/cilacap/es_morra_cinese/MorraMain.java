06e9f4bb46592a3f4a15a63d91dca3aeb98e04eb
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-01-16 20:26:24 +0100

    Padawan09 Commit 16/01

--- src/it/beije/cilacap/es_morra_cinese/MorraMain.java
+++ src/it/beije/cilacap/es_morra_cinese/MorraMain.java
@@ -8,49 +8,52 @@ public class MorraMain {
 	public static void main(String[] args) {
 		int scegli = 0;
 		while(true){
-			System.out.println("premi 1 per 1vs1, 2 per 1vsCPU\n(0 per sasso, 1 per carta, 2 per forbice)");
-			 scegli = sc.nextInt();
-			 if(scegli==1) partita1vs1();
-			 if(scegli==2) partita1vsCPU();
+			System.out.println("premi 1 per 1vs1, 2 per 1vsCPU\n0 per sasso, 1 per carta, 2 per forbice (se numero>2 verrà effettuato il modulo)");
+			scegli = sc.nextInt();
+			if(scegli==1) partita1vs1();
+			if(scegli==2) partita1vsCPU();
 		}
 	}
 	
 	private static void partita1vsCPU() {
 		System.out.println("Giocatore:");
 		int a = sc.nextInt();
+		a=a%3;
 		Random r = new Random();
-		int cpu = r.nextInt()%3;
-		partitaComp(a, cpu);
+		int cpu = r.nextInt(4);
+		partita(a, cpu);
 	}
-	
 	private static void partita1vs1() {
 		System.out.println("Giocatore 1:");
-		int a = sc.nextInt();
+		int a = sc.nextInt()%3;
 		System.out.println("Giocatore 2:");
-		int b = sc.nextInt();
+		int b = sc.nextInt()%3;
 		partita(a, b);
 	}
 	
 	private static void partita(int a, int b) {
-		if (a%3>b%3) System.out.println("Vince giocatore 1!");
-		else if (a%3<b%3) System.out.println("Vince giocatore 2!");
-		else System.out.println("Pareggio!");
-	}
-	
-	private static void partitaComp(int a, int b) {
-		int c = vincitore(a,b);
-		if (c==1) System.out.println("Vince giocatore 1 con "+getType(a)+" su "+getType(b));
-		else if (c==2) System.out.println("Vince giocatore 2 con "+getType(b)+" su "+getType(a));
-		else if (c==0) System.out.println("Pareggio con "+getType(a));
+		switch(vincitore(a,b)) {
+			case 0:
+				System.out.println("Pareggio con "+getType(a));
+				break;
+			case 1:
+				System.out.println("Vince giocatore 1 con "+getType(a)+" su "+getType(b));
+				break;
+			case 2: 
+				System.out.println("Vince giocatore 2 con "+getType(b)+" su "+getType(a));
+				break;
+			default:
+				System.out.println("errore");
+		}
 	}
 	
 	private static String getType(int a) {
-		switch(a%3) {
+		switch(a) {
 		case 0: return "Sasso";
 		case 1: return "Carta";
 		case 2: return "Forbice";
-		default: return "";
 		}
+		return "";
 	}
 	
 	private static int vincitore(int a, int b) {
@@ -64,10 +67,8 @@ public class MorraMain {
 				else return 2;
 			case 0:
 				if(b==2) return 1;
-				else return 2;
-			default:
-				return -1;
-				
+				else return 2;				
 		}
+		return -1;
 	}
 }
