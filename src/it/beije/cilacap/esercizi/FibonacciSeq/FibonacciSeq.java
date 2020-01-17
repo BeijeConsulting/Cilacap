b25b431236364c1ef7e8585f18c5632ff359f23a
Padawan08 <Padawan08@DESKTOP-M1E38VF>
2020-01-17 11:35:22 +0100

    Commit finale esercizio Fibonacci

--- src/it/beije/cilacap/esercizi/FibonacciSeq/FibonacciSeq.java
+++ src/it/beije/cilacap/esercizi/FibonacciSeq/FibonacciSeq.java
@@ -1,12 +1,15 @@
 package it.beije.cilacap.esercizi.FibonacciSeq;
+import java.util.*;
+
+import it.beije.cilacap.esercizi.EserciziStringMethod;
 
 public class FibonacciSeq {
 	
-	int[] seq = null;
+	
 
 	
 	public int[] fibonacciSeq(int index) {
-		
+		int[] seq ;
 		seq = new int[index];
 		
 		if (index < 2) {
@@ -30,16 +33,41 @@ public class FibonacciSeq {
 		return seq;
 	}
 	
-	public void fibonacciSeqRawPrint(int[] seq) {
-		ArrayList<String> seqPrintSxToDx = new ArrayList <> ();
-		ArrayList<String> seqPrintDxToSx = new ArrayList <> ();
+	public String[] fibonacciSeqRawPrint(int[] seq) {
+		String[] seqPrint = new String[seq.length];
+		int x;
 		
-		for (int i = 0; i < seq.length; i++) {
-			seqPrintSxToDx.set(i,"  " + seq[i]);
-			seqPrintDxToSx.set((seq.length-1) - i,"  " + seq[(seq.length-1)-i]);
+		for(int i = 0; i < seq.length; i++) {
+			seqPrint[i] = "";
 		}
 		
+		for(int i = seq.length-1; i >= 0; i--) {
+			for (x = 0; x<=i; x++) {
+				seqPrint[i] += seq[x] + " ";
+			}
+		}
 		
+		return seqPrint;
+	}
+	
+	public void fiboList(int index) {
+		String[] seqPrint = fibonacciSeqRawPrint(fibonacciSeq(index));
+		for(int i = 0 ; i < seqPrint.length; i++) 
+			System.out.println(seqPrint[i] + " " + myReverse(seqPrint[((seqPrint.length-1)-i)]));
+	}
+	//myReverse(seqPrint[((seqPrint.length-1)-i)]));
+	public void fiboRaw(int index) {
+		String[] seqPrint = fibonacciSeqRawPrint(fibonacciSeq(index));
+		System.out.println(seqPrint[index-1]);
+	}
+	
+	public String myReverse(String str1) {
+		StringBuilder response = new StringBuilder();
+		
+		for (int i = str1.length()-1; i >= 0; i--) 
+			response.append(Character.toString(str1.charAt(i)));
+		
+		return response.toString();
 	}
 }
 	
--- src/it/beije/cilacap/esercizi/FibonacciSeq/Main.java
+++ src/it/beije/cilacap/esercizi/FibonacciSeq/Main.java
@@ -5,11 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FibonacciSeq seq = new FibonacciSeq();
-		int[] fiboSeq ;
-		
-		seq.fibonacciSeqRawPrint(seq.fibonacciSeq(5));
-		
-		
+		seq.fiboRaw(13);		
+		seq.fiboList(5);
 	}
 
 }
