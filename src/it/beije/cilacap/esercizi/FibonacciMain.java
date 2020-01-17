f1d7214eb2b59be20daee0c79f447dd812d9d33d
Padawan13 <Padawan13@pc-beije-13>
2020-01-17 16:38:11 +0100

    Fibonacci Conclusp

---
+++ src/it/beije/cilacap/esercizi/FibonacciMain.java
@@ -0,0 +1,15 @@
+package it.beije.cilacap.esercizi;
+
+
+public class FibonacciMain {
+
+	public static void main(String[] args) {
+
+		Fibonacci fb = new Fibonacci();
+		int dim= 4;
+//fb.occorrenza(fb.algorithm(dim), dim);
+//		fb.stringa(fb.algorithm(dim), dim);
+		fb.stringa(fb.algorithm(dim), dim);
+	}
+
+}
