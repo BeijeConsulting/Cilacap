38e6e8a6d4d7de14b03b1abcfe344dd78dab785d
Padawan12 <Padawan12@DESKTOP-39B7B7P>
2020-01-17 17:34:07 +0100

    Ho finito MazzoCarte

--- src/it/beije/cilacap/esercizi/Fibonacci.java
+++ src/it/beije/cilacap/esercizi/Fibonacci.java
@@ -1,16 +1,21 @@
 package it.beije.cilacap.esercizi;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Fibonacci
 {
 	public static void main(String[] args)
 	{
 		int dim = 100; //dimensione array
 		int nocc = 1; //numero occorrenze
+		int nocc2 = 5; //numero occorrenze al contrario
 		int riga = 1; //indica la riga di cui voglio l'occorrenza
 		int[] fibonacci = new int[dim];
 		fibonacci = calcolo(dim);
-		occorrenze(fibonacci, nocc);
-		righe(fibonacci, riga);
+		//occorrenze(fibonacci, nocc);
+		occorrenzedoppie(fibonacci, nocc2); //Non va
+		//righe(fibonacci, riga);
 		
 	}
 	
@@ -49,4 +54,18 @@ public class Fibonacci
 		
 	}
 	
+	public static void occorrenzedoppie(int[] array, int volte)
+	{
+		for (int i=0; i<volte; i++)
+		{
+			int cont = 0;
+			int cont2 = i+1;
+			while (cont < i+1)
+			{
+				System.out.print(array[cont] + " ");
+				cont++;
+			}
+			System.out.println();
+		}
+	}
 }
---
+++ src/it/beije/cilacap/esercizi/MazzoCarte.java
@@ -0,0 +1,32 @@
+package it.beije.cilacap.esercizi;
+
+public class MazzoCarte
+{
+	public static void main(String[] args)
+	{
+		String player1 = ("A");
+		String player2 = ("K");
+		boolean result;
+		String deck = new String();
+		deck = inizializzazione();
+		result = gioco(deck, player1, player2);
+		System.out.println("Il giocatore 1 vince: " + result);
+		
+		
+	}
+	
+	public static String inizializzazione ()
+	{
+		String carte = ("A2345678910JQK");
+		return carte;
+	}
+
+	public static boolean gioco (String mazzo, String carta1, String carta2)
+	{
+		boolean risultato = false;
+		if (mazzo.indexOf(carta1)>mazzo.indexOf(carta2)) risultato = true;
+		if (mazzo.indexOf(carta1) == 0) risultato = true;
+		if (mazzo.indexOf(carta2) == 0) risultato = false;
+		return risultato;
+	}
+}
