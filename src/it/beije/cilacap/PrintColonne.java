302f25173f9b6314083fb0cd0e0f0eb9206fad67
Padawan01 <Padawan01@DESKTOP-V9346E5>
2020-01-21 17:11:57 +0100

    recap

--- src/it/beije/cilacap/PrintColonne.java
+++ src/it/beije/cilacap/PrintColonne.java
@@ -4,6 +4,10 @@ public class PrintColonne {
 
 	
 	public static void main(String[] args) {
+		File f=new File("csv/rubrica1.csv");
+		System.out.println("Il file è "+f.exist());
+		FileReader fileReader=new FileReader(f);
+		BufferedReader reader=new BufferedReader(fileReader);
 		
 	}
 }
