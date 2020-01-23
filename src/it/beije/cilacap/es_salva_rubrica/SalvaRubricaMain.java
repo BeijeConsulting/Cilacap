7dcafd1b223f3e80f60711d92f92ef1c3986bd16
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-01-23 14:15:09 +0100

    Commit Padawan09 23/01

--- src/it/beije/cilacap/es_salva_rubrica/SalvaRubricaMain.java
+++ src/it/beije/cilacap/es_salva_rubrica/SalvaRubricaMain.java
@@ -1,6 +1,8 @@
 package it.beije.cilacap.es_salva_rubrica;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,27 +11,48 @@ public class SalvaRubricaMain {
 	
 	private static final String[] intestazioneDesiderata = {"cognome", "nome", "telefono", "email"};
 	private static Scanner scan = new Scanner(System.in);
-	private static final StringBuilder content = new StringBuilder();
+	private static StringBuilder content;
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
 		File f = new File("Rubrica.txt");
 		if(f.exists()) {
-			System.out.println("file già esistente, sovrascrivere? (S/N)");
-			if(scan.next().equalsIgnoreCase("N")) return;
+			System.out.println("file già esistente, aggiungere i contatti alla rubrica esistente? (S/N)");
+			if(scan.next().equalsIgnoreCase("s")) {
+				aggiungiContattoEsistente(f);
+				return;
+			}
 		}
+		content = new StringBuilder();
 		for(String s : intestazioneDesiderata) {
 			content.append(s+";");
 		}
 		content.append("\n");
-		try {
-			aggiungiContatto(f);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("Si è verificato un errore");
-		}
+		aggiungiContatto(f);
 	}
 	
+	private static void aggiungiContattoEsistente(File f) throws IOException {
+		content = new StringBuilder();
+		FileReader fileReader = new FileReader(f);
+		int c;
+		while((c=fileReader.read())>-1) content.append((char) c);
+		fileReader.close();
+		for(String intestazione : intestazioneDesiderata) {
+			System.out.println("inserisci i dati del campo "+intestazione);
+			content.append(scan.next()+";");
+		}
+		content.append("\n");
+		System.out.println("Vuoi aggiungere un altro campo?(S/N)");
+		if(scan.next().equalsIgnoreCase("S")) aggiungiContattoEsistente(f);
+		else {
+			FileWriter fw = new FileWriter(f);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.append(content.toString());
+			bw.flush();
+			bw.close();
+			scan.close();
+		}
+	}
+
 	private static void aggiungiContatto(File f) throws IOException {
 		for(String intestazione : intestazioneDesiderata) {
 			System.out.println("inserisci i dati del campo "+intestazione);
