e1d4e5d9daf173aefa9f498165f09cfd6dfe7a1f
Padawan10 <Padawan10@DESKTOP-JP18TAH>
2020-01-24 14:21:09 +0100

    File Reader CSV Complete

--- src/it/beije/cilacap/exercises/FileExercise.java
+++ src/it/beije/cilacap/exercises/FileExercise.java
@@ -5,111 +5,75 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-
-
 public class FileExercise {
 
 	public static void main(String[] args) throws IOException {
 		
-		File sheet = new File("csv/rubrica1.csv");
+		File sheet = new File("csv/rubrica4.csv"); // import the file
 		
 		//System.out.println("sheet.exists() ? "+ sheet.exists());
 		//System.out.println("sheet.getAbsolutePath() : "+ sheet.getAbsolutePath());
 		
-		FileReader fileReader = new FileReader(sheet);
+		FileReader fileReader = new FileReader(sheet); // read the file
 		
-		BufferedReader reader = new BufferedReader(fileReader);
-		String firstRow;
-		String secondRow;
-		String otherRows;
-		int[] arrayIndex = new int[5];
+		BufferedReader reader = new BufferedReader(fileReader); // 	read the file line per line
 		
-  		int counterColumns;
-  		int counterData;
-		int j;
 		
-		//Separating the first line of the list and creating indexes for each column
+		String row = reader.readLine();
+		String[] fields = row.split(";");
+		final int[] arrayIndex = new int[5];
+  		int counterColumns;       // Variable to count number of columns in the first line
+  		
 		
-		firstRow = reader.readLine();
-		String[] fields = firstRow.split(";");
+		//Creating indexes for each column
+		
+		//row = reader.readLine();
+	
 		for(counterColumns = 0; counterColumns <= fields.length-1; counterColumns++) {
+			
 			if (fields[counterColumns].contentEquals("NOME")) {
-				arrayIndex[counterColumns] = 0;
+				arrayIndex[0] = counterColumns;
 			}
 			if (fields[counterColumns].contentEquals("COGNOME")) {
-				arrayIndex[counterColumns] = 1;
+				arrayIndex[1] = counterColumns;
 			}
 			if (fields[counterColumns].contentEquals("TELEFONO")) {
-				arrayIndex[counterColumns] = 2;
+				arrayIndex[2] = counterColumns;
 			}
 			if (fields[counterColumns].contentEquals("EMAIL")) {
-				arrayIndex[counterColumns] = 3;
+				arrayIndex[3] = counterColumns;
 			}
 			if (fields[counterColumns].contentEquals("INDIRIZZO")) {
-				arrayIndex[counterColumns] = 4;
-			}
-			System.out.println(fields[counterColumns]);
-		}
-		
-		// Count number of data entrances
-		
-		secondRow = reader.readLine();
-		String[] data = secondRow.split(";");
-		for(counterData = 0; counterData <= data.length - 1; counterData++) {
-			
-		}
-		System.out.println("");
-		
-		System.out.println(counterData);
-		
-		System.out.println("");
-		
-		//Separating the rest of the lines
-		
-		while ((otherRows = reader.readLine()) != null) {
-			
-			String[] arrayData = otherRows.split(";");
-			
-			for(j = 0; j <=arrayData.length-1; j++) {
-				
-				if (j <= counterColumns) {
-				//arrayData[j] =  ;
-				System.out.println(arrayData[j]);
-				}
-				
-				/*
-				for(j = 0; j <=arrayData.length-1; j++) {
-			
-				if (fields[counter].equals("NOME") {
-					System.out.println("NOME: " + array[0]);
-				}
-				if (fields[counter].equals("COGNOME") {
-					System.out.println("COGNOME: " + array[0]);
-				}
-				if (fields[counter].equals("TELEFONO") {
-					System.out.println("TELEFONO: " + array[0]);
-				}
-				if (fields[counter].equals("EMAIL") {
-					System.out.println("EMAIL: " + array[0]);
-				}
-				if (fields[counter].equals("INDIRIZZO") {
-					System.out.println("INDIRIZZO: " + array[0]);
-				}
-				} */	
+				arrayIndex[4] = counterColumns;
 			}
 			
 		}
 		
-		/*
+		row = reader.readLine();
 		
-		if (array[0].equals("")) {}
+		while (row != null) {
+			
+			fields = row.split(";");
+				
+				if (arrayIndex[0] < fields.length) {
+					System.out.println("Nome: " + fields[arrayIndex[0]]);
+				}
+				if (arrayIndex[1] < fields.length) {
+					System.out.println("Cognome: " + fields[arrayIndex[1]]);
+				}
+				if (arrayIndex[2] < fields.length) {
+					System.out.println("Telefono: " + fields[arrayIndex[2]]);
+				}
+				if (arrayIndex[3] < fields.length) {
+					System.out.println("Email: " + fields[arrayIndex[3]]);
+				}
+				if (arrayIndex[4] < fields.length) {
+					System.out.println("Indirizzo: " + fields[arrayIndex[4]]);
+				}
+				
+			row = reader.readLine();
+			}
 		
-		System.out.println("NOME: " + array[0]);
-		System.out.println("COGNOME: " + array[1]);
-		System.out.println("TELEFONO: " + array[2]);
-		System.out.println("EMAIL: " + array[3]);
-		System.out.println("INDIRIZZO: " + array[4]);
-		*/
 		reader.close();	
 	}
 		
