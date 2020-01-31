7da5d3c45f58368ec3584e354bb769fbd559f34e
Clark <clarke2897@gmail.com>
2020-01-31 16:18:19 +0100

    completato import in db da csv e da xml

--- src/it/beije/cilacap/rubrica/EsercizioDB.java
+++ src/it/beije/cilacap/rubrica/EsercizioDB.java
@@ -8,22 +8,93 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import javax.swing.text.DefaultEditorKit.InsertContentAction;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
+import org.xml.sax.SAXException;
+
 import static it.beije.cilacap.esercizi.rubrica.EsercizioRubrica.writeInFileCSV;
-import static it.beije.cilacap.esercizi.rubrica.EsercizioRubrica.writeInXML;;
+import static it.beije.cilacap.esercizi.rubrica.EsercizioRubrica.writeInXML;
+import static it.beije.cilacap.esercizi.rubrica.EsercizioRubrica.loadContactListFromCSV;
+import static it.beije.cilacap.rubrica.ParserXML.getContattiFromFile;
+
 
 public class EsercizioDB {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, ParserConfigurationException, TransformerException {
-		List<Contatto> listaContatti=new ArrayList<Contatto>();
-		listaContatti=DBtools.leggiContatti();
-		File f1=new File("csv/database.csv");
-		File f2=new File("xml/database.xml");
-		writeInFileCSV(listaContatti, f1);
-		writeInXML(listaContatti, f2);
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, ParserConfigurationException, TransformerException, SAXException {
+		System.out.println("Cosa vuoi fare?");
+		System.out.println("1-leggere il database");
+		System.out.println("2-esportare il db in un csv");
+		System.out.println("3- esportare il db in un xml");
+		System.out.println("4- importare il file csv in db");
+		System.out.println("5- importare il file xml in db");
+		
+		Scanner s=new Scanner(System.in);
+		int risposta= s.nextInt();
+		
+		switch(risposta) {
+			case 1:
+				List<Contatto> listaContatti=new ArrayList<Contatto>();
+				listaContatti=DBtools.leggiContatti();
+				break;
+			case 2:
+				File f1=new File("csv/database.csv");
+				List<Contatto> listaContatti2=new ArrayList<Contatto>();
+				listaContatti2=DBtools.leggiContatti();
+				writeInFileCSV(listaContatti2, f1);
+				break;
+			case 3:
+				File f2=new File("xml/database.xml");
+				List<Contatto> listaContatti3=new ArrayList<Contatto>();
+				listaContatti3=DBtools.leggiContatti();
+				writeInXML(listaContatti3, f2);
+				break;
+			case 4:
+				File f3=new File("csv/rubrica.csv");
+				fromCSVToDatabase(f3);
+				break;
+			case 5:
+				File f4=new File("xml/rubrica.xml");
+
+				fromXMLoDatabase(f4);
+				break;
+			default: break;
+				
+		
+		
+		}
+		
+		
+		
+		
+		
+		
 	}
 	
+	public static void fromCSVToDatabase(File f1) throws IOException, ClassNotFoundException {
+		List<Contatto> listaContatti=new ArrayList <Contatto>();
+		listaContatti=loadContactListFromCSV(f1);
+		
+		for(int i=0; i< listaContatti.size();i++) {
+			DBtools.insertContatto(listaContatti.get(i));
+		}
+		
+		
+		
+	}
+	
+	public static void fromXMLoDatabase(File f1) throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException {
+		List<Contatto> listaContatti=new ArrayList <Contatto>();
+		listaContatti=getContattiFromFile(f1);
+		
+		for(int i=0; i< listaContatti.size();i++) {
+			DBtools.insertContatto(listaContatti.get(i));
+		}
+		
+		
+		
+	}
 }
--- src/it/beije/cilacap/rubrica/ParserXML.java
+++ src/it/beije/cilacap/rubrica/ParserXML.java
@@ -1,11 +1,13 @@
 package it.beije.cilacap.rubrica;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -14,6 +16,7 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 
 public class ParserXML {
@@ -24,7 +27,7 @@ public class ParserXML {
 		return getContattiFromFile(file);
 	}
 
-	public static List<Contatto> getContattiFromFile(File file) throws Exception {
+	public static List<Contatto> getContattiFromFile(File file) throws ParserConfigurationException, SAXException, IOException {
 		List<Contatto> listaContatti = new ArrayList<Contatto>();
 		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
