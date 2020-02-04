5c1afe373d875b6352145f0837a477a8ae66982d
Padawan11 <Padawan11@DESKTOP-MQ1AMB6>
2020-02-04 17:14:48 +0100

    Esercizio Scrittura file Csv (Aggiunta sovrascrittura da finire)

--- src/it/beije/cilacap/EsScritturaFile.java
+++ src/it/beije/cilacap/EsScritturaFile.java
@@ -6,15 +6,19 @@ import java.io.*;
 public class EsScritturaFile{
 	
 	static String[] Credenziali = {"Nome", "Cognome", "Telefono", "Email", "Indirizzo"};
-	static String[] CredenzialiLette = new String[5];
+	static String[] CredenzialiLette = new String[Credenziali.length];
+	static ArrayList<String> CredenzialiSalvate = new ArrayList<>();
 	static Scanner input = new Scanner (System.in);
-	static 	boolean scriviAncora = true;
 	static String path = null;
+	static 	boolean scriviAncora = true;
+	static int count = 0;
 	
 	public static void main(String[] args) throws IOException {
-	int count = 0;
-	CreaFile();
-	File file = new File(path);
+	
+	File file = PrendiPercorsoFile();
+	FileReader fr = new FileReader(file);
+	BufferedReader br = new BufferedReader(fr);
+	GestioneFile(file, br);
 	FileWriter fw = new FileWriter(file);
 	BufferedWriter bw = new BufferedWriter(fw);
 	
@@ -22,17 +26,18 @@ public class EsScritturaFile{
 		if(count == 0) {
 			LeggiDaConsole();
 			ScritturaCredenziali(bw);
-			ScritturaCredenzialiLette(bw);
+			ScritturaCredenzialiLette(br, bw);
 			ScriviAncora();
 			count++;
 		} else if (scriviAncora == true){
 			LeggiDaConsole();
-			ScritturaCredenzialiLette(bw);
+			ScritturaCredenzialiLette(br, bw);
 			ScriviAncora();
 		}
 	}
 	bw.flush();
 	bw.close();
+	br.close();
 }
 	
 	public static void LeggiDaConsole() {
@@ -59,8 +64,10 @@ public class EsScritturaFile{
 }
 	
 	
-	public static void ScritturaCredenzialiLette(BufferedWriter bw) throws IOException {
+	public static void ScritturaCredenzialiLette(BufferedReader br, BufferedWriter bw) throws IOException {
+		System.out.println(br.readLine());
 		
+		while(br.readLine() != null){
 		for(int i = 0; i < Credenziali.length; i++) {
 			
 			if(i == (Credenziali.length - 1)) {
@@ -72,9 +79,9 @@ public class EsScritturaFile{
 			}
 		}
 	}
+}
 	
 	public static void ScriviAncora() {
-		
 		boolean rimani = true;
 		
 		do {
@@ -97,39 +104,52 @@ public class EsScritturaFile{
 		} while(rimani);
 	}
 	
-	public static void CreaFile() {
-		boolean rimani = true;
-
+	public static File PrendiPercorsoFile() throws IOException {
+		
 		System.out.println("Inserisci percorso file");
 		path = input.nextLine();
-		try {
 		File file = new File(path);
-
+		
 		if (file.exists()) {
-		System.out.println("Il file " + path + " esiste,");
-		System.out.println("Vuoi sovrascrivere?\tSi\tNo");
+			System.out.println("Il file " + path + " esiste,");
+		} else if (file.createNewFile()) {
+			System.out.println("Il file " + path + " è stato creato");
+		} else {
+			System.out.println("Il file " + path + " non può essere creato");
+		}
+		
+		return file;
+	}
+	
+	public static void GestioneFile(File file, BufferedReader br) throws IOException {
+		boolean rimani = true;
+		
+		if (file.exists()) {
+		System.out.println("Vuoi:");
+		System.out.println("1. Continuare su questo file");
+		System.out.println("2. sovrascrivere");
 		String risposta = input.nextLine();
 		
 			do {
-				if(risposta.equalsIgnoreCase("Si")) {
-					file.createNewFile();
-					System.out.println("Il file " + path + " è stato sovrascritto correttamente");
+				if(risposta.equals("1")) {
+					String row = br.readLine();
+					while(row != null) {
+						System.out.println(row);
+						CredenzialiSalvate.add(row);
+						row = br.readLine();
+					}
+					
+					System.out.println(CredenzialiSalvate);
 					rimani = false;
-				} else if(risposta.equalsIgnoreCase("No")) {
+				} else if(risposta.equals("2")) {
+					System.out.println("Il file " + path + " è stato sovrascritto correttamente");
+					file.createNewFile();
 					rimani = false;
 				} else {
 					rimani = true;
 				}
-			} while(rimani);
-			
-		} else if (file.createNewFile()) {
-		System.out.println("Il file " + path + " è stato creato");
-		} else {
-		System.out.println("Il file " + path + " non può essere creato");
+			} while(rimani);	
 		}
-		
-		}catch (IOException e) {
-		e.printStackTrace();
-		}			
 	}
 }
+
