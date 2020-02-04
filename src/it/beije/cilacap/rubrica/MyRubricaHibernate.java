fbaf1d82d0c3c8eeb573030296c3802f709ce451
0gintama0 <gabry19942009@gmail.com>
2020-02-04 14:46:05 +0100

    wdad

---
+++ src/it/beije/cilacap/rubrica/MyRubricaHibernate.java
@@ -0,0 +1,12 @@
+package it.beije.cilacap.rubrica;
+
+import java.util.List;
+
+public class MyRubricaHibernate {
+	
+	
+	public static void inserisciContatto(List<Contatto> listaContatti) {
+		
+	}
+
+}
