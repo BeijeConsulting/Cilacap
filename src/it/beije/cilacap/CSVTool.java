714116fb15a69061ced6f37c995ac407d284d3de
Padawan11 <Padawan11@DESKTOP-MQ1AMB6>
2020-02-07 16:36:04 +0100

    CSVTool Lettura/Scrittura FINITO

--- src/it/beije/cilacap/CSVTool.java
+++ src/it/beije/cilacap/CSVTool.java
@@ -9,17 +9,18 @@ import it.beije.cilacap.rubrica.Contatto;
 public class CSVTool {
 	
 	static List<Contatto> listaContatti = new ArrayList<Contatto>();
-	static Contatto contatto = new Contatto();
 	
 	public static void main(String[] args) throws IOException {
 		
-		String path = "csv/copia1.txt";
-		ReaderCSV(path);
-//		WriterCSV(listaContatti);
+//		String path = "csv/copia1.txt";
+//		listaContatti = ReaderCSV(path);
+//		path = "C:/work/Provaa.txt";
+//		WriterCSV(path, listaContatti);
 	}
 
 	//Legge file CSV
 	public static List<Contatto> ReaderCSV(String path) throws IOException{	
+		
 		File file = new File(path);
 		FileReader fileReader = new FileReader(file);
 		BufferedReader reader = new BufferedReader(fileReader);
@@ -28,13 +29,14 @@ public class CSVTool {
 		String[] credenziali = new String[10];
 		int count = 0;
 
+		Contatto contatto = null;
 		while ((row = reader.readLine()) != null) {
+			contatto = new Contatto();
 			
 			String[] header = row.split(";");
 
 			if (count == 0) {
 				for (int i = 0; i < header.length; i++) {
-					System.out.println(header[i]);
 					credenziali[i] = header[i];
 				}
 				count++;
@@ -43,47 +45,57 @@ public class CSVTool {
 				
 				for (int i = 0; i < header.length; i++) {
 					
-					if(credenziali[i].equalsIgnoreCase("nome")) {
+					if(credenziali[i].equalsIgnoreCase("nome"))
 						contatto.setNome(header[i]);
-					} else if(credenziali[i].equalsIgnoreCase("cognome")) {
+					else if(credenziali[i].equalsIgnoreCase("cognome")) 
 						contatto.setCognome(header[i]);
-					} else if(credenziali[i].equalsIgnoreCase("telefono")) {
+					else if(credenziali[i].equalsIgnoreCase("telefono")) 
 						contatto.setTelefono(header[i]);
-					} else if(credenziali[i].equalsIgnoreCase("email")) {
+					else if(credenziali[i].equalsIgnoreCase("email")) 
 						contatto.setEmail(header[i]);
-					} else {
-						System.out.println("Errore");
-					}
+					
 				}
 				listaContatti.add(contatto);
-				System.out.println(contatto);
-//				System.out.println(listaContatti);
 			}
 		}
-		System.out.println(listaContatti);
 		reader.close();
 		return listaContatti;
 	}
 	
 	
-	public static void WriterCSV(List<Contatto> listaContatti) throws IOException{
+	//Scrive file CSV
+	public static void WriterCSV(String path, List<Contatto> listaContatti) throws IOException{
 		
-		String[] arrayContatto = new String[5];
-		int j = 0;
+		ArrayList<String> arrayContatti = new ArrayList<>();
 		
-		while(j < listaContatti.size()) {
-		for (int i = 1; i <= listaContatti.size(); i++) {
-				
-				if(i == 2)
-				System.out.println(contatto.getCognome());	
-				else if(i == 3)
-				System.out.println(contatto.getNome());
-				else if(i == 4)
-				System.out.println(contatto.getTelefono());		
-				else if(i == 5)
-				System.out.println(contatto.getEmail());
-			}
-		j++;
+		File file = new File(path);
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		String assemblaContatto = null;
+		for(Contatto contatto : listaContatti) {
+			
+			assemblaContatto = contatto.getId() + ";" +
+							   contatto.getCognome() + ";" +
+							   contatto.getNome() + ";" +
+							   contatto.getTelefono() + ";" +
+							   contatto.getEmail();
+			
+			arrayContatti.add(assemblaContatto);
 		}
+		
+		int count = 0;
+		for(String cursore : arrayContatti) {
+			
+			if(count == 0) {
+				bw.write("ID;COGNOME;NOME;TELEFONO;EMAIL\n");
+				++count;
+			}
+			
+			bw.write(cursore + "\n");
+			System.out.println(cursore);
+		}
+		bw.flush();
+		bw.close();
 	}
 }
